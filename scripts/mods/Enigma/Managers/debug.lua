4e67522fc82ceec7dffd53f298f7c7be26c350de
Vermintide Analytics <VermintideAnalytics@gmail.com>
2023-12-26 22:06:20 -0500

    Fix debug sphere not disappearing when dismissed

--- scripts/mods/Enigma/Managers/debug.lua
+++ scripts/mods/Enigma/Managers/debug.lua
@@ -52,8 +52,14 @@ end
 enigma:command("enigma_draw_aura", "Draw a sphere of a specific radius around you. If no radius provided, hides the sphere", function(radius)
     if not radius then
         dm.aura_radius = nil
+        LineObject.reset(dm.line_object)
+        LineObject.dispatch(dm.line_object_world, dm.line_object)
         enigma:echo("Debug aura visualization turned off")
         return
+    end
+    if dm.aura_radius then
+        LineObject.reset(dm.line_object)
+        LineObject.dispatch(dm.line_object_world, dm.line_object)
     end
     dm.aura_radius = radius
     if dm.line_object then
@@ -79,11 +85,12 @@ end)
 dm.update = function(self, dt)
     if self.aura_radius then
         local local_player_unit = enigma:local_player_unit()
-        local center = Unit.world_position(local_player_unit, 0)
-        LineObject.add_sphere(self.line_object, self.aura_color:unbox(), center, self.aura_radius, 50, 10)
-        LineObject.dispatch(self.line_object_world, self.line_object)
-        LineObject.reset(self.line_object)
+        if local_player_unit then
+            local center = Unit.world_position(local_player_unit, 0)
+            LineObject.add_sphere(self.line_object, self.aura_color:unbox(), center, self.aura_radius, 50, 10)
+            LineObject.dispatch(self.line_object_world, self.line_object)
+            LineObject.reset(self.line_object)
+        end
     end
-
 end
 enigma:register_mod_event_callback("update", dm, "update")
